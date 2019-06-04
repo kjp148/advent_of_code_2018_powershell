91abdc1aa4d5d914603ac258e451f1bfb588c12e
kjp148 <keananpaul@yahoo.com>
2019-06-04 03:10:02 -0700

    Finished day 2 part 2

--- day2.ps1
+++ day2.ps1
@@ -20,3 +20,42 @@ $BoxIDs | ForEach-Object {
 
 Write-Host "< Day 2 Part 1 >"
 Write-Host "Checksum:" ($TotalDoubleIDs * $TotalTripleIDs)
+Write-Host
+
+# Part 2
+$FirstID = ""
+$SecondID = ""
+$Differences = 0
+
+# Find matching ID's
+$BoxIDs | ForEach-Object {
+    $ID = $_
+    $BoxIDs | ForEach-Object {
+        $CompareID = $_
+        for ($i = 0; $i -lt $ID.length; $i++)
+        {
+            if ($ID[$i] -ne $CompareID[$i]) {$Differences++}
+        }
+
+        if ($Differences -eq 1)
+        {
+            $FirstID = $ID
+            $SecondID = $CompareID
+        }
+        $Differences = 0
+    }
+}
+
+# Remove extra char from matching ID's
+for ($i = 0; $i -lt $ID.length; $i++)
+{
+    if ($FirstID[$i] -ne $SecondID[$i])
+    {
+        $CorrectID = $FirstID.Remove($i, 1)
+        break
+    }
+}
+
+Write-Host "< Day 2 Part 2 >"
+Write-Host "Correct ID:" $CorrectID
+Write-Host
